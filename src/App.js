7238a8b3beaccd57802e1c0931d866bf16dee6dd
atiradoGit88 <angeltirado@pursuit.org>
2023-05-24 21:12:53 -0400

    added components.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// Do not add .gitignore or .env
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
