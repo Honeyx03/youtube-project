a74848d9a976ae18386e13eeae7b33b669edc39b
Noni Porter <n@nonis-air.mynetworksettings.com>
2023-05-31 19:34:50 -0400

    Set-up to Nav and links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // Do not add .gitignore or .env
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+/*
+components
+*/
+import NavBar from './components/NavBar';
+import Home from './components/Home/Home.js';
+import About from './components/About.js';
+
 function App() {
+  const [teamMembers] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Nav />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About teamMembers={teamMembers}/>}></Route>
+      </Routes>
+    </Router>
+  </>
   );
 }
 
