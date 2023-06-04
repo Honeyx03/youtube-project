5991684d7ccd1d44812cf38bc71688cfec74a7d4
atiradoGit88 <angeltirado@pursuit.org>
2023-06-04 16:15:40 -0400

    Created css for the videos and limited the videos to 4 videos.

--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -1,14 +1,19 @@
 import { useEffect,useState } from "react";
 import YouTube from 'react-youtube';
+import "./VideoList.css";
 const URL = process.env.REACT_APP_API_KEY;
 
 export default function VideosList({searchVideo}) {
     const [allVideos, setAllVideos] = useState([])
     
-    console.log(searchVideo)
-    console.log(allVideos)
+    
+        
+
     useEffect(() => {
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchVideo}&kind=video&type=video&maxResults=10&key=${URL}
+        if (searchVideo === "") {
+            console.log ("error")
+        } else {
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchVideo}&kind=video&type=video&maxResults=4&key=${URL}
         `).then((response) => response.json())
         .then(data => {
             setAllVideos(data.items) 
@@ -17,15 +22,23 @@ export default function VideosList({searchVideo}) {
             console.error(error)
         }
         )
-
+        }
       },[searchVideo])
 
+
+
+
+
   return (
-    <div>
-        <h1> {allVideos.map((video, i) => {
-            // return <p key={i}>{video.snippet.title}</p>
-            return <YouTube videoId={video.id.videoId}/>
-        })}</h1>
+    <div className="col-md-4 container">
+        <div className="row">
+            {allVideos.length < 1 ? null : allVideos.map((video, i) => {
+                // return <p key={i}>{video.snippet.title}</p>
+                return (<div className="col-sm-6 col-md-6 col-lg-6" key={video.id.videoId}>
+                    <YouTube videoId={video.id.videoId} opts={{ width: "100%", height: "auto" }}/> </div>)
+            })}
+           
+        </div>
     </div>
   );
 }
