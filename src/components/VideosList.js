741112c16451ded2db52e641e73ec79984418cbc
antunishdPursuit <dennysantunish@pursuit.org>
2023-06-05 19:59:29 -0400

    Added route path for video/id

--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -1,14 +1,12 @@
 import { useEffect,useState } from "react";
-import YouTube from 'react-youtube';
+import { Link } from "react-router-dom";
 import "./VideoList.css";
+import Video from "./Video";
 const URL = process.env.REACT_APP_API_KEY;
 
 export default function VideosList({searchVideo}) {
     const [allVideos, setAllVideos] = useState([])
     
-    
-        
-
     useEffect(() => {
         if (searchVideo === "") {
             console.log ("error")
@@ -25,19 +23,17 @@ export default function VideosList({searchVideo}) {
         }
       },[searchVideo])
 
-
-
-
-
   return (
     <div className="col-md-4 container">
         <div className="row">
             {allVideos.length < 1 ? null : allVideos.map((video, i) => {
                 // return <p key={i}>{video.snippet.title}</p>
-                return (<div className="col-sm-6 col-md-6 col-lg-6" key={video.id.videoId}>
-                    <YouTube videoId={video.id.videoId} opts={{ width: "100%", height: "auto" }}/> </div>)
+                return (
+                <div className="col-sm-6 col-md-6 col-lg-6" key={video.id.videoId}>
+                    <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
+                    <Video video={video}></Video>
+                </div>)
             })}
-           
         </div>
     </div>
   );
